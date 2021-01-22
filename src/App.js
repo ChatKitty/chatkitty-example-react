904e6a6dad3c66cc71a8ef63f0974113a37ef59d
Aaron Nwabuoku <aerisvulpe@gmail.com>
2021-01-21 21:38:24 -0500

    Style login screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './themes/main.scss';
+
 import Providers from './navigation';
 
 export default function App() {
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import { Loader } from '@chatscope/chat-ui-kit-react';
 import React, { useContext, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
@@ -13,56 +14,58 @@ export const LoginScreen = () => {
   const { login, loading } = useContext(AuthContext);
 
   return (
-    <Card className="login">
-      <Card.Body>
-        <Form>
-          <h1 className="mb-3">Login to your account</h1>
-          <Form.Group>
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              name="username"
-              type="text"
-              placeholder="username"
-              onChange={(event) => setUsername(event.target.value)}
-              autoComplete="username"
-            />
-          </Form.Group>
+    <Container className="d-flex h-100 flex-column align-items-center justify-content-center">
+      <Card className="login">
+        <Card.Body>
+          <Form>
+            <h1 className="mb-3">Login to your account</h1>
+            <Form.Group>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                name="username"
+                type="text"
+                placeholder="username"
+                onChange={(event) => setUsername(event.target.value)}
+                autoComplete="username"
+              />
+            </Form.Group>
 
-          <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              name="password"
-              type="password"
-              placeholder="password"
-              onChange={(event) => setPassword(event.target.value)}
-              autoComplete="current-password"
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                type="password"
+                placeholder="password"
+                onChange={(event) => setPassword(event.target.value)}
+                autoComplete="current-password"
+              />
+            </Form.Group>
+            <Button
+              size="sm"
+              className="w-100"
+              onClick={() => login(username, password)}
+              disabled={username.length === 0 || password.length === 0}
+            >
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+        {loading === true && (
+          <>
+            <div
+              className="w-100 h-100 position-absolute d-flex align-items-center justify-content-center bg-light"
+              style={{ zIndex: '10', opacity: '0.4' }}
             />
-          </Form.Group>
-          <Button
-            size="sm"
-            className="w-100"
-            onClick={() => login(username, password)}
-            disabled={username.length === 0 || password.length === 0}
-          >
-            Login
-          </Button>
-        </Form>
-      </Card.Body>
-      {loading === true && (
-        <>
-          <div
-            className="w-100 h-100 position-absolute d-flex align-items-center justify-content-center bg-light"
-            style={{ zIndex: '10', opacity: '0.4' }}
-          />
-          <div
-            className="w-100 h-100 position-absolute d-flex align-items-center justify-content-center"
-            style={{ zIndex: '11' }}
-          >
-            <Loader />
-          </div>
-        </>
-      )}
-    </Card>
+            <div
+              className="w-100 h-100 position-absolute d-flex align-items-center justify-content-center"
+              style={{ zIndex: '11' }}
+            >
+              <Loader />
+            </div>
+          </>
+        )}
+      </Card>
+    </Container>
   );
 };
 
